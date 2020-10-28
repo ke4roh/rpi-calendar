40a9cd026a8f50d6efee7e398dec3436b4631388
Jim Scarborough <jimes@hiwaay.net>
2020-10-28 19:26:51 -0400

    Fixes #4 Add PIR sensor

--- roles/calendar/files/startup.sh
+++ roles/calendar/files/startup.sh
@@ -4,4 +4,7 @@
 # [ $(lsusb | wc -l) -lt 4 ] && echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
 
 # Start the browser
-chromium-browser --app=https://calendar.google.com/calendar/r/custom/7/d --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201" --disable-infobars --disable-session-crashed-bubble --kiosk &
+nohup chromium-browser --app=https://calendar.google.com/calendar/r/custom/7/d --user-agent="Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201" --disable-infobars --disable-session-crashed-bubble --kiosk </dev/null 2>&1 >/dev/null  &
+
+# If blanking is requested, then run pirMonitor to make motion keep it from blanking
+grep -q noblank /etc/xdg/lxsession/LXDE-pi/autostart || nohup /home/pi/pirMonitor </dev/null 2>&1 >/dev/null &
